5de96ee9ef593e72e657bae1cbe5a0ffdd71c923
Imp-insp <vitorbadotti28@gmail.com>
2025-10-02 02:25:01 -0300

    checkPoint anim worm lights

--- AntJam/Assets/Scripts/Enemies/Worm.cs
+++ AntJam/Assets/Scripts/Enemies/Worm.cs
@@ -1,13 +1,21 @@
+using System;
 using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class Worm : MonoBehaviour
 {
-   [Header("Patrol Settings")] public Transform[] waypoints; // List of patrol points
+   [Header("Patrol Settings")] 
+   public Transform[] waypoints; // List of patrol points
     [SerializeField] private float speed = 2f; // Movement speed
     [SerializeField] private float restingTime = 2f;
     private bool resting;
     private Rigidbody2D rb;
+    
+    [Header("Light")] 
+    [SerializeField] private Light2D lig2D;
+    [SerializeField] private LayerMask groundMask;
 
     
     void Start()
@@ -16,9 +24,12 @@ public class Worm : MonoBehaviour
         if (waypoints.Length > 0)
             transform.position = waypoints[0].position; // Start at first waypoint
     }
-
+    
     void FixedUpdate()
     {
+        
+        var hit = Physics2D.CircleCast(transform.position, 1f,transform.up, 0.3f, groundMask);
+        lig2D.enabled = !hit.collider;
         if (!resting)
         {
             if (waypoints.Length < 2) return;
@@ -39,10 +50,12 @@ public class Worm : MonoBehaviour
 
     private IEnumerator ResetToFirstWaypoint()
     {
-        transform.position = waypoints[0].position;
+        
         resting = true;
         yield return new WaitForSeconds(restingTime);
+        transform.position = waypoints[0].position;
         resting = false;
+        
     }
 
     private void OnDrawGizmosSelected()
@@ -53,5 +66,20 @@ public class Worm : MonoBehaviour
             if (waypoints[i] != null && waypoints[i + 1] != null)
                 Gizmos.DrawLine(waypoints[i].position, waypoints[i + 1].position);
         }
+    }
+
+    
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer($"Ground"))
+        {
+            lig2D.enabled = false;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        lig2D.enabled = true;
     }
 }
--- AntJam/Assets/Scripts/Player/PlayerManager.cs
+++ AntJam/Assets/Scripts/Player/PlayerManager.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections;
+using DG.Tweening;
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
@@ -7,7 +10,14 @@ public class PlayerManager : MonoBehaviour
 {
     public static PlayerManager Instance;
 
+    [Header("Animation")] [SerializeField] private float checkAnimDuration;
+    [SerializeField] private float centerOffSet;
+    [SerializeField] private float centerDuration;
+    
+    
     [Header("Ui")] [SerializeField] private GameObject pauseMenu;
+    [SerializeField] private TextMeshProUGUI checkPointText;
+    private Camera _mainCamera;
 
     [Header("Save")] [SerializeField] private Vector2 lastPos;
     [SerializeField] private Quaternion lastRot;
@@ -24,6 +34,8 @@ public class PlayerManager : MonoBehaviour
 
     private void Start()
     {
+        _mainCamera =  Camera.main;
+        
         lastPos = transform.position;
         lastRot = transform.rotation;
     }
@@ -37,8 +49,22 @@ public class PlayerManager : MonoBehaviour
 
     public void SetCheckPoint(Vector2 newPos, Quaternion newRot)
     {
+        if (newPos == lastPos) return;
+        StartCoroutine(CheckPointAnimation());
         lastPos = newPos;
         lastRot = newRot;
+    }
+
+    private IEnumerator CheckPointAnimation()
+    {
+        checkPointText.transform.DOLocalMoveX(0/*-centerOffSet*/, checkAnimDuration);
+            yield return new WaitForSeconds(checkAnimDuration);
+            /*checkPointText.transform.DOLocalMoveX(0+centerOffSet, centerDuration);*/
+            yield return new WaitForSeconds(centerDuration);
+            checkPointText.transform.DOLocalMoveX(_mainCamera.pixelWidth, checkAnimDuration);
+            yield return new WaitForSeconds(checkAnimDuration);
+            checkPointText.transform.position = new Vector2(-_mainCamera.pixelWidth, checkPointText.transform.position.y);
+
     }
 
     public void PauseMenu()
